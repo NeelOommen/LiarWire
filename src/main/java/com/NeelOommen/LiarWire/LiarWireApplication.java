20a261b68ef3bee040d4e1672d0eafb46d0cec64
NeelOommen <neelittyerahoommen@gmail.com>
2025-05-21 19:47:20 +0530

    Initial commit

---
+++ src/main/java/com/NeelOommen/LiarWire/LiarWireApplication.java
@@ -0,0 +1,13 @@
+package com.NeelOommen.LiarWire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiarWireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiarWireApplication.class, args);
+	}
+
+}
