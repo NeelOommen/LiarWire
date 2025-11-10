37a267d7a980159285dd4b9edcf7ae1a08ac4b44
NeelOommen <neelittyerahoommen@gmail.com>
2025-11-10 22:00:38 +0530

    Fixed Delay implementation, removed Thread.sleep to avoid thread starvation

--- src/main/java/com/tls/LiarWire/services/DelayService.java
+++ src/main/java/com/tls/LiarWire/services/DelayService.java
@@ -6,11 +6,6 @@ import java.util.Random;
 
 public abstract class DelayService {
 
-    public void delayResponse(MockApiConfig config) throws InterruptedException {
-        double delayPercentile = getRandomPercentage();
-        Thread.sleep(getDelayinMillisBasedOnPercentile(config, delayPercentile));
-    }
-
     public double getRandomPercentage(){
         Random random = new Random();
         return random.nextInt(101);
